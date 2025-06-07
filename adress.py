ecd6cc937587eeb638078126f1c56bf8b040eb84
akusokekaterina <akusokekaterina@gmail.com>
2025-06-08 01:08:59 +0700

    Delete adress.py

--- adress.py
+++
@@ -1,9 +0,0 @@
-class Adress:
-    def __init__(self, index, city, street, house, comnat):
-        self.index = index
-        self.city = city
-        self.street = street
-        self.house = house
-        self.comnat = comnat
-    def __str__(self):
-        return f"{self.index} {self.city} {self.street} {self.house} - {self.comnat}"
