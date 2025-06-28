bc8b790d515c9edc2bc42e0e792c615c0fe73883
akusokekaterina <akusokekaterina@gmail.com>
2025-06-28 19:43:02 +0700

    Add files via upload

---
+++ lesson_05/lesson05_task1.py
@@ -0,0 +1,19 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(
+service=ChromeService(ChromeDriverManager().install()))
+
+driver.maximize_window()
+
+driver.get("http://uitestingplayground.com/classattr")
+
+for i in range(5):
+    button = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
+    button.click()
+
+# Ждем 25 секунд, чтобы видеть результат клика
+sleep(25)
