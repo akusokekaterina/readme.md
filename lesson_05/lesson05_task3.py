9b9bb40eed00bf5de0f357bced359f69154ba28e
akusokekaterina <akusokekaterina@gmail.com>
2025-07-07 17:40:06 +0700

    Add files via upload

---
+++ lesson_05/lesson05_task3.py
@@ -0,0 +1,21 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install())
+)
+
+driver.maximize_window()
+
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+input_box = driver.find_element(By.TAG_NAME, "input")
+input_box.send_keys("Sky")
+input_box.clear()
+input_box.send_keys("Pro")
+sleep(10)
+
+driver.quit()
---
+++ lesson_05/lesson05_task4.py
@@ -0,0 +1,27 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install())
+)
+
+driver.maximize_window()
+
+driver.get("http://the-internet.herokuapp.com/login")
+
+username_input = driver.find_element(By.NAME, "username")
+username_input.send_keys("tomsmith")
+password_input = driver.find_element(By.NAME, "password")
+password_input.send_keys("SuperSecretPassword!")
+
+driver.find_element(By.CSS_SELECTOR, "button [type='submit']").click()
+sleep(2)
+
+success_message = driver.find_element(By.CLASS_NAME, "flash.success")
+
+print(success_message.text)
+
+driver.quit()
